9e6a5cba3441a2f177eb145cf8e04a36d7f4f761
GABRIEL EDUARDO SOARES TEIXEIRA GONCALVES FARINHA <202001208811@alunos.estacio.br>
2023-12-20 17:22:37 -0300

    first commit

---
+++ bissecção.py
@@ -0,0 +1,36 @@
+# Metodo da Bissecção
+def bissec(a, b, e):
+    Nvezes = 1
+    condicao = True
+    while condicao:
+        resul = (a + b) / 2
+        print("Interação- %d, X= %0.6f e a função f(X)= %0.6f" % (Nvezes, resul, f(resul)))
+
+        # Teorema de Bolzano
+        if f(a) * f(resul) < 0:
+            b = resul
+        else:
+            a = resul
+
+        # Contador
+        Nvezes += 1
+        condicao = abs(f(resul)) > e
+
+    print("\n A raiz aproximada e de: %0.8f" % resul)
+
+
+# Input
+f_string = input("Digite a função f(x): ")
+# Replace 'P' and 'O' with their respective values before evaluating the function
+f_string = f_string.replace('P', '3.1415').replace('O', '1.61803')
+f = eval('lambda x: ' + f_string)
+
+a = float(input("Digite o primeiro chute: "))
+b = float(input("Digite o segundo chute: "))
+e = 1e-5
+
+# Raiz
+if f(a) * f(b) > 0:
+    print("Os chutes não têm raiz. Tente novamente.")
+else:
+    bissec(a, b, e)
